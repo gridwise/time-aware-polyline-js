ab6519577effaf0f0070afdd81e1efa0006f8428
Arjun Attam <arjunattam@gmail.com>
2016-08-30 13:39:23 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,87 @@
+// Code goes here
+
+function lshiftOperator(num, bits) {
+  // Custom left shift for 64 bit integers
+  return num * Math.pow(2, bits);
+}
+
+function rshiftOperator(num, bits) {
+  // Custom right shift for 64 bit integers
+  return Math.floor(num / Math.pow(2, bits));
+}
+
+function notOperator(num) {
+  // Custom not operator for 64 bit integers
+  return ~num;
+}
+
+function getDecodedDimensionFromPolyline(polyline, index) {
+  // Method to decode one dimension of the polyline
+  var result = 1;
+  var shift = 0;
+
+  while (true) {
+    var polylineChar = polyline[index];
+    var b = polylineChar.charCodeAt(0) - 63 - 1;
+    index ++;
+    result += lshiftOperator(b, shift);
+    shift += 5;
+    
+    if (b < 0x1f) {
+      break;
+    }
+  }
+
+  if ((result % 2) !== 0) {
+    return [index, rshiftOperator(notOperator(result), 1)];
+  } else {
+    return [index, rshiftOperator(result, 1)];
+  }
+}
+
+function decodeTimeAwarePolyline(polyline) {
+  // Method to decode a time aware polyline and return gpx logs
+  var gpxLogs = [];
+  var index = 0;
+  var lat = 0;
+  var lng = 0;
+  var timeStamp = 0;
+  var polylineLine = polyline.length;
+  
+  while (index < polylineLine) {
+    // Decoding dimensions one by one
+    var latResult = getDecodedDimensionFromPolyline(polyline, index);
+    index = latResult[0];
+    var lngResult = getDecodedDimensionFromPolyline(polyline, index);
+    index = lngResult[0];
+    var timeResult = getDecodedDimensionFromPolyline(polyline, index);
+    index = timeResult[0];
+    
+    // Resultant variables
+    lat += latResult[1];
+    lng += lngResult[1];
+    timeStamp += timeResult[1];
+    gpxLogs.push(getGpxLog(lat, lng, timeStamp));
+  }
+  
+  return gpxLogs;
+}
+
+function getCoordinate(coordinate) {
+  return coordinate * 0.00001;
+}
+
+function getIsoTime(timeStamp) {
+  return new Date(timeStamp).toISOString();
+}
+
+function getGpxLog(lat, lng, timeStamp) {
+  return [
+    getCoordinate(lat), getCoordinate(lng), getIsoTime(timeStamp)
+  ];
+}
+
+var polyline = "spxsBsdb|L}_ni~jotAx@T{uJvAr@avJ";
+var decodedPolyline = decodeTimeAwarePolyline(polyline);
+
+
