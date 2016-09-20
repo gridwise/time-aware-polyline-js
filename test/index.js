40cd79d2e8bf6187d35eb3abc7744daa48ad7af0
Arjun Attam <arjunattam@gmail.com>
2016-09-20 00:08:12 -0700

    Add new method to get elapsed locations till timestamp

--- test/index.js
+++ test/index.js
@@ -2,7 +2,8 @@ var should = require('chai').should(),
     polyline = require('../index'),
     decoder = polyline.decodeTimeAwarePolyline,
     encoder = polyline.encodeTimeAwarePolyline,
-    locationFinder = polyline.getLocationsAtTimestamps;
+    locationFinder = polyline.getLocationsAtTimestamps,
+    locationsElapsed = polyline.getLocationsElapsedByTimestamp;
 
 var points = [
     [19.13626, 72.92506, '2016-07-21T05:43:09.000Z'],
@@ -10,7 +11,13 @@ var points = [
     [19.13553, 72.92469, '2016-07-21T05:43:21.000Z']
 ];
 
-var polyline = 'spxsBsdb|Lymo`qvAx@TKvAr@K';
+var pointsLocations = [];
+
+for (var i = 0; i < points.length; i++) {
+  pointsLocations.push([points[i][0], points[i][1]]);
+}
+
+var encoded = 'spxsBsdb|Lymo`qvAx@TKvAr@K';
 
 var timeStampsToFind = [
     '2016-07-21T05:43:21.000Z', // exact match with third
@@ -28,18 +35,24 @@ var locationsFound = [
 
 describe('#decoder', function() {
   it('decodes polyline', function() {
-    decoder(polyline).should.deep.equal(points);
+    decoder(encoded).should.deep.equal(points);
   });
 
   it('finds locations', function() {
     locationFinder(
-      polyline, timeStampsToFind
+      encoded, timeStampsToFind
     ).should.deep.equal(locationsFound);
-  })
+  });
+
+  it('locations traveled', function() {
+    locationsElapsed(
+      encoded, timeStampsToFind[0]
+    ).should.deep.equal(pointsLocations);
+  });
 });
 
 describe('#encoder', function() {
   it('encodes polyline', function() {
-    encoder(points).should.equal(polyline);
+    encoder(points).should.equal(encoded);
   });
 });
